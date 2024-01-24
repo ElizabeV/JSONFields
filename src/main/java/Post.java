132f9746836df9b8ded3bbb1484a5ca9f2c2f33c
Elizabeth Viricheva <liza.vir@ya.ru>
2024-01-24 19:44:17 +0300

    json formed

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    //FormDate formdate = new FormDate();
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription; // добавьте другие поля
+}
